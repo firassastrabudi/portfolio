6944c1a6934c9c56b70d247fb27c60e468c65f22
amirakbarwicaksono <amirakbarwicaksono@gmail.com>
2025-08-07 09:18:08 +0700

    Change headline

--- src/resources/content.js
+++ src/resources/content.js
@@ -55,7 +55,7 @@ const home = {
   label: "Home",
   title: `${person.name}'s Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
-  headline: <>Advancing drone technology and savoring culinary artistry.</>,
+  headline: <>Hopefully advancing drone technology and savoring culinary artistry.</>,
   featured: {
     display: true,
     title: <>Recent project: <strong className="ml-4">Once UI</strong></>,
